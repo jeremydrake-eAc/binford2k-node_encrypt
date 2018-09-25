b9a05db4a1a3992cb97c11c738b0f937d668f252
Jeremy Drake <jeremydrake+github@eacceleration.com>
2018-09-25 16:38:26 -0700

    Revert "use require_relative"
    Trying to support older versions of puppet and ruby.
    
    This reverts commit 3f39d562265403a11de8116b1a804ac3edcd3f1f.

--- lib/puppet/parser/functions/node_encrypt.rb
+++ lib/puppet/parser/functions/node_encrypt.rb
@@ -1,4 +1,4 @@
-require_relative '../../../puppet_x/binford2k/node_encrypt'
+require File.join(File.dirname(__FILE__), '../../..', 'puppet_x/binford2k/node_encrypt.rb')
 
 Puppet::Parser::Functions::newfunction(:node_encrypt,
   :type  => :rvalue,
